1b72c652e192b70f845b36f572f6d25d7c354b58
PSH <gyak404@gmail.com>
2024-11-04 17:21:09 +0900

    hotfix: argument resolver 수정

--- backend/techpick-security/src/main/java/techpick/security/util/LoginUserArgumentResolver.java
+++ backend/techpick-security/src/main/java/techpick/security/util/LoginUserArgumentResolver.java
@@ -2,24 +2,15 @@ package techpick.security.util;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import techpick.security.annotation.LoginUserId;
-import techpick.security.config.SecurityConfig;
 import techpick.security.exception.ApiOAuth2Exception;
 
-@Component
-@RequiredArgsConstructor
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
-
-	private final CookieUtil cookieUtil;
 
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
@@ -38,11 +29,6 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 		if (authentication != null && authentication.getPrincipal() instanceof Long) {
 			return authentication.getPrincipal();
 		}
-
-		HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-		HttpServletResponse response = webRequest.getNativeResponse(HttpServletResponse.class);
-		cookieUtil.deleteCookie(request, response, SecurityConfig.ACCESS_TOKEN_KEY);
-		cookieUtil.deleteCookie(request, response, SecurityConfig.LOGIN_FLAG_FOR_FRONTEND);
 
 		throw ApiOAuth2Exception.INVALID_AUTHENTICATION();
 	}
