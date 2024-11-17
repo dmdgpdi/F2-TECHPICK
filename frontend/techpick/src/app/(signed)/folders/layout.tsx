4e2446ece59da24f19ea8564e8718ee65d634c0f
kimminkyeu <minkyeki@student.42seoul.kr>
2024-11-17 18:23:12 +0900

    feat: 무한 스크롤 다듬기 작업

--- frontend/techpick/src/app/(signed)/folders/layout.tsx
+++ frontend/techpick/src/app/(signed)/folders/layout.tsx
@@ -1,7 +1,14 @@
 import type { PropsWithChildren } from 'react';
 import { FolderTree, FolderAndPickDndContextProvider } from '@/components';
+import { CurrentPathIndicator } from '@/components/FolderPathIndicator/CurrentPathIndicator';
 import { SearchWidget } from '@/components/SearchWidget/SearchWidget';
-import { pageContainerLayout, ListViewerLayout } from './layout.css';
+import {
+  pageContainerLayout,
+  ListViewerLayout,
+  ListViewerHeaderLayout,
+  ListViewerHeaderMainLayout,
+  ListViewerHeaderSubLayout,
+} from './layout.css';
 
 export default function FolderLayout({ children }: PropsWithChildren) {
   return (
@@ -9,7 +16,14 @@ export default function FolderLayout({ children }: PropsWithChildren) {
       <FolderAndPickDndContextProvider>
         <FolderTree />
         <div className={ListViewerLayout}>
-          <SearchWidget />
+          <div className={ListViewerHeaderLayout}>
+            <div className={ListViewerHeaderMainLayout}>
+              <SearchWidget />
+            </div>
+            <div className={ListViewerHeaderSubLayout}>
+              <CurrentPathIndicator />
+            </div>
+          </div>
           {children}
         </div>
       </FolderAndPickDndContextProvider>
