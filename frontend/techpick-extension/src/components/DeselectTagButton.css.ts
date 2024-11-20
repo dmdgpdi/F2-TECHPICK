2e76828c4afbae4f8e2f1f1d6042255f4c4cceb6
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-11-20 11:14:57 +0900

    [DESIGN] 디자인 변경 (#500)

--- frontend/techpick-extension/src/components/DeselectTagButton.css.ts
+++ frontend/techpick-extension/src/components/DeselectTagButton.css.ts
@@ -2,6 +2,9 @@ import { style } from '@vanilla-extract/css';
 import { colorVars } from 'techpick-shared';
 
 export const DeselectTagButtonStyle = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
   width: '20px',
   height: '20px',
   backgroundColor: 'transparent',
