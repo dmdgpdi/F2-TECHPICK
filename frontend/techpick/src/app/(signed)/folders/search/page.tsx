d962b1cc57ed04f1398e0f550204a433d02d0e6c
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-11-21 16:39:33 +0900

    [FIX] 두번 useEffect 호출 되는 거 방지 (#518)

--- frontend/techpick/src/app/(signed)/folders/search/page.tsx
+++ frontend/techpick/src/app/(signed)/folders/search/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { PickListViewerInfiniteScroll } from '@/components/PickListViewer/PickListViewerInfiniteScroll';
 import { usePickStore } from '@/stores/pickStore/pickStore';
@@ -10,21 +10,25 @@ export default function SearchPickResultPage() {
   const searchParams = useSearchParams();
   const { getSearchResult, searchPicksByQueryParam } = usePickStore();
   const [pickList, setPickList] = useState<PickListType>([]);
+  const isLoadFirst = useRef(true);
 
-  useEffect(() => {
-    setPickList([]);
-    (async () => {
-      await loadNextSlice();
-    })(/*IIFE*/);
-  }, [searchParams]);
-
-  const loadNextSlice = async () => {
+  const loadNextSlice = useCallback(async () => {
     await searchPicksByQueryParam(
       searchParams.toString(),
       getSearchResult().lastCursor
     );
     setPickList((prev) => prev.concat(getSearchResult().content));
-  };
+  }, [getSearchResult, searchParams, searchPicksByQueryParam]);
+
+  useEffect(() => {
+    if (isLoadFirst.current) {
+      isLoadFirst.current = false;
+      setPickList([]);
+      (async () => {
+        await loadNextSlice();
+      })(/*IIFE*/);
+    }
+  }, [loadNextSlice, searchParams]);
 
   return (
     <PickListViewerInfiniteScroll
