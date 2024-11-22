68c1447532ec356aa1ce8643d3d979760ab85414
sangwonsheep <sangwonsheep@gmail.com>
2024-11-22 18:09:52 +0900

    fix: 액션 테스트용 커밋

--- backend/techpick-api/src/main/java/techpick/TechPickApiApplication.java
+++ backend/techpick-api/src/main/java/techpick/TechPickApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 	scanBasePackages = {"techpick.api", "techpick.core", "techpick.security"}
 )
 
-
+// 테스트를 위한 주석 - 제거 예정
 public class TechPickApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TechPickApiApplication.class, args);
