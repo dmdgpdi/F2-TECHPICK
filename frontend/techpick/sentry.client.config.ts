a65200f2b29d0de27baaac02b52f839eb885ab03
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-12-11 17:16:14 +0900

    hotfix: 빌드 실패로 인한 sentry 재설정

--- frontend/techpick/sentry.client.config.ts
+++ frontend/techpick/sentry.client.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/namespace */
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
@@ -7,19 +8,7 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: 'https://1d6c7e3ae35f9bb6335a01e68eb11fc9@o4508387512745984.ingest.us.sentry.io/4508387515301888',
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
-  /**
-   * 에러가 발생할 때 즉시 녹화가 시작되어 지속되는 리플레이 샘플 속도입니다.
-   * 오류 전 최대 1분동안 이벤트를 기록합니다.
-   * 1.0 - 0 사이의 값을 사용합니다. (1.0 권장)
-   */
-  replaysOnErrorSampleRate: 1.0,
-
+  // Add optional integrations for additional features
   integrations: [
     Sentry.replayIntegration({
       /**
@@ -32,4 +21,18 @@ Sentry.init({
       blockAllMedia: false,
     }),
   ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  /**
+   * 에러가 발생할 때 즉시 녹화가 시작되어 지속되는 리플레이 샘플 속도입니다.
+   * 오류 전 최대 1분동안 이벤트를 기록합니다.
+   * 1.0 - 0 사이의 값을 사용합니다. (1.0 권장)
+   */
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 });
